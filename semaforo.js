3158fc67da1bcf3ea579c4455967473c2960cff0
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2024-11-10 14:28:09 -0300

    Inicio de projeto

---
+++ semaforo.js
@@ -0,0 +1,6 @@
+const img = document.getElementById("img");
+const buttons = document.getElementById("button");
+
+const trafficLght = (event) => {};
+
+buttons.addEventListener("click", trafficLght);
